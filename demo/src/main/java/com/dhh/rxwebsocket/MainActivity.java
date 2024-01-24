f20a42531d93ad1f48968ff1d843184ebfe4534d
nino9012 <ysy1987@gmail.com>
2024-01-24 15:24:48 +0800

    commit

--- demo/src/main/java/com/dhh/rxwebsocket/MainActivity.java
+++ demo/src/main/java/com/dhh/rxwebsocket/MainActivity.java
@@ -1,6 +1,7 @@
 package com.dhh.rxwebsocket;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
@@ -16,8 +17,11 @@ import com.dhh.websocket.RxWebSocket;
 import com.dhh.websocket.WebSocketInfo;
 import com.dhh.websocket.WebSocketSubscriber;
 import com.dhh.websocket.WebSocketSubscriber2;
+import com.google.gson.Gson;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import okhttp3.Response;
 import okhttp3.WebSocket;
@@ -56,28 +60,37 @@ public class MainActivity extends AppCompatActivity {
 
         RxWebSocket.setConfig(config);
         // please use WebSocketSubscriber
-        RxWebSocket.get("your url")
+        RxWebSocket.get("wss://eoaptest.cebbank.com/uiap/wss")
                 //RxLifecycle : https://github.com/dhhAndroid/RxLifecycle
                 .compose(RxLifecycle.with(this).<WebSocketInfo>bindOnDestroy())
                 .subscribe(new WebSocketSubscriber() {
                     @Override
                     public void onOpen(@NonNull WebSocket webSocket) {
-                        Log.d("MainActivity", "onOpen1:");
+                        Log.d("RXXXXX", "onOpen1:");
+                        startHeart();
+                        sendLogin();
                     }
 
                     @Override
                     public void onMessage(@NonNull String text) {
-                        Log.d("MainActivity", "返回数据:" + text);
+                        Log.d("RXXXXX", "返回数据:" + text);
                     }
 
                     @Override
                     public void onMessage(@NonNull ByteString byteString) {
+                        Log.d("RXXXXX", "返回数据:" + byteString);
 
                     }
 
                     @Override
                     protected void onReconnect() {
-                        Log.d("MainActivity", "重连:");
+                        Log.d("RXXXXX", "重连:");
+                    }
+
+                    @Override
+                    protected void onClose() {
+                        Log.d("RXXXXX", "onClose:");
+                        super.onClose();
                     }
                 });
         /**
@@ -85,24 +98,91 @@ public class MainActivity extends AppCompatActivity {
          *如果你想将String类型的text解析成具体的实体类，比如{@link List<String>},
          * 请使用 {@link  WebSocketSubscriber2}，仅需要将泛型传入即可
          */
-        RxWebSocket.get("your url")
-                .compose(RxLifecycle.with(this).<WebSocketInfo>bindToLifecycle())
-                .subscribe(new WebSocketSubscriber2<List<String>>() {
-                    @Override
-                    protected void onMessage(List<String> strings) {
-
-                    }
-                });
-
-        mSubscription = RxWebSocket.get("ws://sdfsd")
-                .subscribe(new WebSocketSubscriber() {
-                    @Override
-                    protected void onClose() {
-                        Log.d("MainActivity", "直接关闭");
-                    }
-                });
+//        RxWebSocket.get("your url")
+//                .compose(RxLifecycle.with(this).<WebSocketInfo>bindToLifecycle())
+//                .subscribe(new WebSocketSubscriber2<List<String>>() {
+//                    @Override
+//                    protected void onMessage(List<String> strings) {
+//
+//                    }
+//                });
+//
+//        mSubscription = RxWebSocket.get("ws://sdfsd")
+//                .subscribe(new WebSocketSubscriber() {
+//                    @Override
+//                    protected void onClose() {
+//                        Log.d("MainActivity", "直接关闭");
+//                    }
+//                });
 
         initDemo();
+    }
+
+    Handler mHandler;
+    Runnable mRunnable;
+
+    Handler offLineHandler;
+    Runnable offLineRunnable;
+
+    private void startHeart() {
+        if (mHandler != null) return;
+        mHandler = new Handler();
+        mRunnable = new Runnable() {
+            @Override
+            public void run() {
+
+                mHandler.postDelayed(this, 10000);
+                sendHeart();
+            }
+
+        };
+        mHandler.post(mRunnable);
+//        if (offLineHandler == null) {
+//            offLineHandler = new Handler();
+//            offLineRunnable = new Runnable() {
+//                @Override
+//                public void run() {
+////                    setOnlineStatus(false);
+//                }
+//            };
+//            offLineHandler.postDelayed(offLineRunnable, 15000);
+//        }
+
+    }
+
+    private void sendHeart() {
+        Map map = new HashMap();
+        map.put("signal", "PING");
+        map.put("subSignal", "NONE");
+        map.put("messageId", 1);// 消息ID
+        map.put("from", "0");// 登录人员类型：0 患者端，1医生端
+        map.put("content", "heartCheck");
+        Gson gson = new Gson();
+        send(gson.toJson(map));
+
+
+    }
+
+    private void sendLogin() {
+        Map map = new HashMap();
+        map.put("signal", "CONNECT_ACK");
+        map.put("subSignal", "NONE");
+        map.put("messageId", 1);// 消息ID
+        map.put("from", "0");// 登录人员类型：0 患者端，1医生端
+        Map content = new HashMap();
+        content.put("userToken", "E76D7A1CE5130EC0E05316D8C80AB0E7");// 登录用户标识
+        content.put("terminal", "ws");
+        content.put("platform", "web");
+        map.put("content", content);
+
+        Gson gson = new Gson();
+        send(gson.toJson(map));
+    }
+
+    public void send(String jsonStr) {
+        Gson gson = new Gson();
+        Log.i("RXXXXX", "send: " + jsonStr);
+        RxWebSocket.asyncSend("wss://eoaptest.cebbank.com/uiap/wss", jsonStr);
     }
 
     @Override
@@ -117,43 +197,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        RxWebSocket.get("url")
-                .subscribe(new WebSocketSubscriber() {
-                    @Override
-                    protected void onMessage(@NonNull String text) {
-
-                    }
-                });
-
-        RxWebSocket.get("your url")
-                //RxLifecycle : https://github.com/dhhAndroid/RxLifecycle
-                .compose(RxLifecycle.with(this).<WebSocketInfo>bindToLifecycle())
-                .subscribe(new WebSocketSubscriber() {
-                    @Override
-                    public void onOpen(@NonNull WebSocket webSocket) {
-                        Log.d("MainActivity", "onOpen1:");
-                    }
-
-                    @Override
-                    public void onMessage(@NonNull String text) {
-                        Log.d("MainActivity", "返回数据:" + text);
-                    }
-
-                    @Override
-                    public void onMessage(@NonNull ByteString byteString) {
-
-                    }
-
-                    @Override
-                    protected void onReconnect() {
-                        Log.d("MainActivity", "重连:");
-                    }
-
-                    @Override
-                    protected void onClose() {
-                        Log.d("MainActivity", "onClose:");
-                    }
-                });
+//        RxWebSocket.get("url")
+//                .subscribe(new WebSocketSubscriber() {
+//                    @Override
+//                    protected void onMessage(@NonNull String text) {
+//
+//                    }
+//                });
+//
+//        RxWebSocket.get("your url")
+//                //RxLifecycle : https://github.com/dhhAndroid/RxLifecycle
+//                .compose(RxLifecycle.with(this).<WebSocketInfo>bindToLifecycle())
+//                .subscribe(new WebSocketSubscriber() {
+//                    @Override
+//                    public void onOpen(@NonNull WebSocket webSocket) {
+//                        Log.d("RXXXXX", "onOpen1:");
+//                    }
+//
+//                    @Override
+//                    public void onMessage(@NonNull String text) {
+//                        Log.d("RXXXXX", "返回数据:" + text);
+//                    }
+//
+//                    @Override
+//                    public void onMessage(@NonNull ByteString byteString) {
+//
+//                    }
+//
+//                    @Override
+//                    protected void onReconnect() {
+//                        Log.d("RXWebSocket", "重连:");
+//                    }
+//
+//                    @Override
+//                    protected void onClose() {
+//                        Log.d("RXWebSocket", "onClose:");
+//                    }
+//                });
     }
 
     private void initDemo() {
@@ -166,7 +246,7 @@ public class MainActivity extends AppCompatActivity {
         setListener();
 
         // unsubscribe
-        Subscription subscription = RxWebSocket.get("ws://sdfs").subscribe();
+        Subscription subscription = RxWebSocket.get("wss://rpc.heyunnetwork.com/diagnosis/socket").subscribe();
         if (subscription != null && !subscription.isUnsubscribed()) {
             subscription.unsubscribe();
         }
